0299ee99239c98dbe6eeb1811f7bfd7b5d498b8b
Nikodem Solarz <narzew@gmail.com>
2021-09-29 10:52:53 +0200

    Add Product scaffold and sample data

---
+++ db/migrate/20210929084450_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :code
+      t.decimal :price, :scale => 2, :precision => 30
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times {
+  Product.create(name: Faker::Food.ingredient, code: Faker::Barcode.ean(13), price: rand(0.01..100.0).round(2))
+}
